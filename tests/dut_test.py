5c8a706157340a8b9e7e2780f0aa2eff33e6a69f
Janavi-G <149292634+Janavi-G@users.noreply.github.com>
2023-11-16 21:54:59 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,13 +1,10 @@
 import cocotb
 from cocotb.triggers import Timer, RisingEdge
-
-
 @cocotb.test()
 async def dut_test(dut):
     a=(0, 0, 1, 1)
     b=(0, 1, 0, 1)
     y=(0, 1, 1, 0)
-
     for i in range(4):
         dut.a.value = a[i]
         dut.a.value = b[i]
