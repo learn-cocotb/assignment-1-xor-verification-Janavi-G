63e2c14d3619e16ca7c5d9fdaac7e6e18e38f5fa
Janavi-G <149292634+Janavi-G@users.noreply.github.com>
2023-11-16 23:42:47 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -7,6 +7,6 @@ async def dut_test(dut):
     y=(0, 1, 1, 0)
     for i in range(4):
         dut.a.value = a[i]
-        dut.a.value = b[i]
+        dut.b.value = b[i]
         await Timer(1,'ns')
         assert dut.y.value == y[i], f"Error at Iteration {i}"
