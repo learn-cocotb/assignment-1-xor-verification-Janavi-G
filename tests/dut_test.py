364e0c3abc87b32f9a109748f15ce718b76aa951
Janavi-G <149292634+Janavi-G@users.noreply.github.com>
2023-11-16 20:19:53 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,8 +1,15 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer,Rising Edge
 
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 1, "Testing assertion"
-    pass
+    a=(0,0,1,1)
+    b=(0,1,0,1)
+    y=(0,1,1,0)
+
+    for in range(4):
+    dut.a.value=a[i]
+    dut.a.value=b[i]
+await Timer(1,'ns')
+assert dut.y.value == y[i],f"Error at Iteration{i}"
