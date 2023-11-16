c4b20f510748984c3a1a30ed928d91cd9b303dda
Janavi-G <149292634+Janavi-G@users.noreply.github.com>
2023-11-16 20:25:00 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -3,13 +3,13 @@ from cocotb.triggers import Timer,RisingEdge
 
 
 @cocotb.test()
-async def dut_test(dut):
-    a=(0,0,1,1)
-    b=(0,1,0,1)
-    y=(0,1,1,0)
+async def xor_test(dut):
+    a=(0, 0, 1, 1)
+    b=(0, 1, 0, 1)
+    y=(0, 1, 1, 0)
 
     for i in range(4):
-    dut.a.value=a[i]
-    dut.a.value=b[i]
-await Timer(1,'ns')
-assert dut.y.value == y[i],f"Error at Iteration{i}"
+        dut.a.value=a[i]
+        dut.a.value=b[i]
+    await Timer(1,'ns')
+    assert dut.y.value == y[i],f"Error at Iteration{i}"
