914566d84665e87cfc8cccfa43175938a025e82c
Janavi-G <149292634+Janavi-G@users.noreply.github.com>
2023-11-17 20:45:33 +0530

    Create wrapperclass.v

---
+++ hdl/wrapperclass.v
@@ -0,0 +1,16 @@
+module xor_wave(
+        input wire a,
+        input wire b,
+        input wire y
+);
+  xor_gate xor_gate(
+    a(a),
+    ,b(b),
+    ,y(y),
+  );
+  initial begin
+    $dumpfile ("waves.vcd")
+    $dumpvars
+  end
+  end module
+    
